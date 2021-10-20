12823b2440a10038f24e952870078829abe68e66
Andrew <konoplyanik0504@gmail.com>
2021-10-20 22:48:11 +0300

    Add project files.

---
+++ MvcStartApp/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MvcStartApp.Models;
+using MvcStartApp.Models.Db;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcStartApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // ссылка на репозиторий
+        private readonly ILogger<HomeController> _logger;
+
+        // Также добавим инициализацию в конструктор
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MvcStartApp/Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MvcStartApp.Models.Db;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcStartApp.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly IBlogRepository _repo;
+
+        public UsersController(IBlogRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(User newUser)
+        {
+            await _repo.AddUser(newUser);
+            return View(newUser);
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var authors = await _repo.GetUsers();
+            return View(authors);
+        }     
+    }
+}
